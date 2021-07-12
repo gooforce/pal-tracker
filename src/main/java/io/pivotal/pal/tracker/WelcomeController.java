d1f6587f55da007921e10289585417830a1b7175
Ubuntu <ubuntu@ip-172-31-47-191.ap-southeast-1.compute.internal>
2021-07-12 10:40:29 +0000

    Configuring an App
    Lab commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    @Value("${welcome.message}")
+    String message;
+
+    public WelcomeController() {
+    }
+
+    public WelcomeController(String msg) {
+        message=msg;
+    }
 
     @GetMapping("/")
     public String sayHello() {
-        return "yes";
+        return message;
     }
 }
