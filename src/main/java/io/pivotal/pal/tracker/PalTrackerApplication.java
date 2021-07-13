a3f88d8292f4c8a2e3d74e45538f727fa135c326
Ubuntu <ubuntu@ip-172-31-47-191.ap-southeast-1.compute.internal>
2021-07-13 08:22:59 +0000

    Spring MVC with REST Endpoints 2

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -12,7 +12,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public InMemoryTimeEntryRepository myInMemoryTimeEntryRepository() {
+    public TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
     }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -15,19 +15,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class TimeEntryController {
 
-    @Autowired
     private TimeEntryRepository timeEntryRepository;
-
-    public TimeEntryController(){
-    }
 
     public TimeEntryController(TimeEntryRepository tmR){
         this.timeEntryRepository = tmR;
