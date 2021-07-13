cb3fc6eee7ac94bfbe14a675a12d95b4738d7446
Ubuntu <ubuntu@ip-172-31-47-191.ap-southeast-1.compute.internal>
2021-07-13 08:45:56 +0000

    Spring MVC with REST Endpoints 3

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class TimeEntryController {
